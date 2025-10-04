6e630f29abd54a58b1a4839d52582ee081421bb1
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2025-10-04 17:23:36 -0400

    chore: initial commit

---
+++ .vscode/scripts/debug-client.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CLIENT_DEV_PID=$(pgrep -f '[d]ebug-client.sh' -d ' ')
+
+# If there is a vite dev server already running, kill it.
+if [[ ! -z $CLIENT_DEV_PID ]] && [[ $CLIENT_DEV_PID != $$ ]]
+then
+	kill $CLIENT_DEV_PID
+fi
+
+pnpm run dev
