9e0386d669eead151be725c3735e740912cda25f
Guillaume Marcoux <guillaumemarcoux@gmail.com>
2025-10-08 02:20:53 -0400

    fix: add proper error page and better french new round button

---
+++ src/routes/+error.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Button } from "$lib/components/ui/button";
+</script>
+
+<div class="grow flex flex-col justify-center items-center gap-5">
+	<p>Oh noes, seems like an error happened :/</p>
+
+	<Button href="/">Go back to home screen</Button>
+</div>
